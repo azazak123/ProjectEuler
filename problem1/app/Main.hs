3f165af7d2addeea8bbb3da5a5415a42caf7cc9d
azazak123 <azazaka2002@gmail.com>
2022-12-28 14:25:04 +0200

    add problem 1 - "Multiples of 3 or 5"

---
+++ problem1/app/Main.hs
@@ -0,0 +1,8 @@
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+module Main where
+
+main :: IO ()
+main = print $
+  (3 + 999) * (999 / 3) / 2 + -- arithmetic progression (d = 3, sum multiples of 3)
+  (5 + 995) * (995 / 5) / 2 - -- arithmetic progression (d = 5, sum multiples of 5)
+  (15 + 990) * (990 / 15) / 2 -- arithmetic progression (d = 15, sum multiples of 3 and 5)
