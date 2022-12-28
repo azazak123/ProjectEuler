3b666fc8af3481bf3b8d6cc669cb48f6aa965bc1
azazak123 <azazaka2002@gmail.com>
2022-12-28 23:27:08 +0200

    format files

--- problem1/app/Main.hs
+++ problem1/app/Main.hs
@@ -1,8 +1,11 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
+
 module Main where
 
 main :: IO ()
-main = print $
-  (3 + 999) * (999 / 3) / 2 + -- arithmetic progression (d = 3, sum multiples of 3)
-  (5 + 995) * (995 / 5) / 2 - -- arithmetic progression (d = 5, sum multiples of 5)
-  (15 + 990) * (990 / 15) / 2 -- arithmetic progression (d = 15, sum multiples of 3 and 5)
+main =
+  print $
+    (3 + 999) * (999 / 3) / 2
+      + (5 + 995) * (995 / 5) / 2 -- arithmetic progression (d = 3, sum multiples of 3)
+      - (15 + 990) * (990 / 15) / 2 -- arithmetic progression (d = 5, sum multiples of 5)
+      -- arithmetic progression (d = 15, sum multiples of 3 and 5)
--- problem3/app/Main.hs
+++ problem3/app/Main.hs
@@ -1,13 +1,14 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
+
 module Main where
 
 main :: IO ()
-main = print . head $ getPrimeFactors 600851475143 [3..] []
+main = print . head $ getPrimeFactors 600851475143 [3 ..] []
 
 getPrimeFactors :: Integral a => a -> [a] -> [a] -> [a]
 getPrimeFactors number arr factors
   | number `quotRem` current == (1, 0) = current : factors
-  | number `mod` current == 0 = getPrimeFactors (number `div` (head arr)) arr (current : factors)
+  | number `mod` current == 0 = getPrimeFactors (number `div` head arr) arr (current : factors)
   | otherwise = getPrimeFactors number (tail arr) factors
   where
     current = head arr
